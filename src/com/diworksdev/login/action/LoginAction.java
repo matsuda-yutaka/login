c4c5b3143e726212e8211f233330d631969a6d6c
matsuda-yutaka <matsudayutaka@matsudayutakas-MacBook-Air.local>
2024-01-12 21:18:57 +0900

    third commit

--- src/com/diworksdev/login/action/LoginAction.java
+++ src/com/diworksdev/login/action/LoginAction.java
@@ -1,22 +1,22 @@
 package com.diworksdev.login.action;
-
 import java.sql.SQLException;
 
 import com.diworksdev.login.dao.LoginDAO;
 import com.diworksdev.login.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LoginAction extends ActionSupport {
+public class LoginAction extends ActionSupport{
+	
 	private String name;
 	private String password;
 	
-	public String execute() throws SQLException {
+	public String execute() throws SQLException{
 		String ret = ERROR;
 		
 		LoginDAO dao = new LoginDAO();
 		LoginDTO dto = new LoginDTO();
 		
-		dto = dao.select(name,password);
+		dto = dao.select(name, password);
 		
 		if(name.equals(dto.getName())) {
 			if(password.equals(dto.getPassword())) {
@@ -37,5 +37,4 @@ public class LoginAction extends ActionSupport {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 }
